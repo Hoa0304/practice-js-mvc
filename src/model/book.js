0035637d64a0b7f3eac37cebc819c36e8f736779
Hoa0304 <chtran030404@gmail.com>
2024-02-28 21:10:24 +0700

    card(#8)

---
+++ src/model/book.js
@@ -0,0 +1,12 @@
+class Book {
+constructor({
+    img, name,author, year, storRate,id=random()
+}){
+        this.img = img;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.storRate = storRate;
+    }
+
+}
