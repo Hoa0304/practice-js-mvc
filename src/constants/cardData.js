1a5dff47c681d092bfa4a456332909ff47eed4e0
Hoa0304 <chtran030404@gmail.com>
2024-03-11 22:25:49 +0700

    constant(#20)

---
+++ src/constants/cardData.js
@@ -0,0 +1,83 @@
+import { harry, holy, learn, react, rich, sprint, things, think } from '../resources/assets/image';
+
+const CardData = [
+    {
+        img : think,
+        name : "Don't Make Me Think",
+        author : "Steve Krug, 2000",
+    },
+    {
+        img : things,
+        name : "The Design of EveryDay Things",
+        author : "Don Norman, 1988",
+    },
+    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },
+    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },
+    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },
+    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },
+    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },
+    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },
+    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },    {
+        img : think,
+        name : "Don't Make Me Think insjlfnhgkbslglbslbl",
+        author : "Steve Krug, 2000",
+    },
+]
+
+export default CardData;
---
+++ src/constants/constant.js
@@ -0,0 +1,16 @@
+const BROWSER = [
+    'Engineering', 
+    'Medical', 
+    'Arts & Science', 
+    'Architecture', 
+    'Law'
+];
+
+const ACCOUNT = [
+    'Profile',
+    'Favorite',
+    'Payments',
+    'Logout'
+]
+
+export { BROWSER, ACCOUNT };
